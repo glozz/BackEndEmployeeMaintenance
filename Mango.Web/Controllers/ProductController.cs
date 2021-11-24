3070bb1806ff03a25a2d6e606beb591cb74fecae
Ntshetse Gloucester <esromglozz@gmail.com>
2021-11-24 09:31:54 +0200

    first commit Add project files.

--- Mango.Web/Controllers/ProductController.cs
+++ Mango.Web/Controllers/ProductController.cs
@@ -2,7 +2,9 @@
 using Mango.Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Mango.Web.Controllers
 {
@@ -12,7 +14,7 @@ namespace Mango.Web.Controllers
 
         public ProductController(IProductServices productionService)
         {
-                _productionService = productionService;
+            _productionService = productionService;
         }
 
         public async Task<IActionResult> ProductIndex()
@@ -26,6 +28,82 @@ namespace Mango.Web.Controllers
             }
 
             return View(list);
+        }
+
+        public async Task<IActionResult> ProductCreate()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductCreate(ProductDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productionService.CreateProductAsync<ResponseDto>(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> EditProduct(int productId)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productionService.GetProductByIdAsync<ResponseDto>(productId);
+                if (response != null && response.IsSuccess)
+                {
+                    ProductDto model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+                    return View(model);
+                }
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditProduct(ProductDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productionService.UpdateProductAsync<ResponseDto>(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> DeleteProduct(int productId)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productionService.GetProductByIdAsync<ResponseDto>(productId);
+                if (response != null && response.IsSuccess)
+                {
+                    ProductDto model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+                    return View(model);
+                }
+            }
+            return NotFound();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProduct(ProductDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productionService.DeleteProductAsync<ResponseDto>(model.ProductId);
+                if (response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+            return View(model);
         }
     }
 }
--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -33,6 +33,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Product}/{action=ProductIndex}/{id?}");
 
 app.Run();
